a9f8e0948a621a26d3468e04917988c615f9ad66
tomatine <tomatine86@gmail.com>
2023-06-28 18:13:38 +0900

    add R example

---
+++ example.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+
+example <- function(a=1, b = 2) {
+
+ 
+ print("This code is bad formatted.")
+ print(1+2/3)
+
+
+        print("This code is bad formatted.")
+
+
+  print("Hello World!")
+}
